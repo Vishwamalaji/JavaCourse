f1e8896d1872d5cc8a697f5d47a066cb285d888e
Vishwamalaji <vishwamalji@gmail.com>
2026-09-21 15:47:26 +0000

    initial commit hello

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class first{
     public static void main(String[]args){
         System.out.println("Hello world!!");
+        System.out.println("This is my first java program");
+
     }
 }
